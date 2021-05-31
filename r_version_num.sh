7d48da2829f9c533948b5ea6e825a8e260f60ae5
AndreMikulec <Andre_Mikulec@Hotmail.com>
2021-05-31 05:26:23 -0500

    simple copy over to r_version_num.txt

--- r_version_num.sh
+++ r_version_num.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 # remove first whitespace and all the way through the end of the line 
 sed -i -r -e 's/\s+.*$//g' $(cygpath "${rsource}")/VERSION
 
-# remove the only and last empty line (if any)
-echo -n $(cat $(cygpath $(cygpath "${rsource}")/VERSION) > ${APPVEYOR_BUILD_FOLDER}/r_version_num.txt
+cp $(cygpath "${rsource}")/VERSION ${APPVEYOR_BUILD_FOLDER}/r_version_num.txt
+cat                                ${APPVEYOR_BUILD_FOLDER}/r_version_num.txt
 
 set +v +x
